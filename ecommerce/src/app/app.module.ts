8ae113f7ffede59e24c9c2741efc47e1f3e7fa9a
zoidee <passakornke@rdpd.aerothai.co.th>
2023-07-18 21:01:04 +0700

    feat(service): add product service

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +10,9 @@ import { CartComponent } from './pages/cart/cart.component';
 import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CartComponent,
-    SaleComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, CartComponent, SaleComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ecommerce/src/app/pages/home/home.component.ts
+++ ecommerce/src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  productList: any[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    debugger;
+    this.loadAllProducts();
+  }
+
+  loadAllProducts() {
+    debugger;
+    this.productService.getAllProducts().subscribe((results: any) => {
+      this.productList = results.data;
+    });
+  }
 }
